f21e3538ce9d3bead421d405ef05627f39b712cb
Florian Frank <flori@ping.de>
2020-01-07 12:46:14 +0100

    stop modifying strings

--- lib/complex_config/plugins/uri.rb
+++ lib/complex_config/plugins/uri.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 module ComplexConfig::Plugins
   URI = -> id do
-    url = id.to_s
+    url = id.to_s.dup
     if url.sub!(/uri\z/, 'url') and url = ask_and_send(url)
       ::URI.parse(url)
     else
--- lib/complex_config/settings.rb
+++ lib/complex_config/settings.rb
@@ -133,7 +133,7 @@ class ComplexConfig::Settings < BasicObject
   def to_s(pair_sep: ' = ', path_sep: ?.)
     empty? and return self.class.name
     pathes(path_sep: path_sep).inject('') do |result, (path, value)|
-      result << "#{path}#{pair_sep}#{value.inspect}\n"
+      result + "#{path}#{pair_sep}#{value.inspect}\n"
     end
   end
 
