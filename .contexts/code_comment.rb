1af4efe26b7e9feb796310ae5e5b250252cd7ffb
Florian Frank <flori@ping.de>
2025-11-24 11:15:45 +0100

    Add bin directory file context handling
    - Added `bin` namespace context to include all files in the `bin` directory
    - Files in `bin` directory are tagged with `bin` tag
    - Uses `Dir['bin/*']` to find all files in the bin directory
    - Maintains existing `lib` namespace context behavior

--- .contexts/code_comment.rb
+++ .contexts/code_comment.rb
@@ -1,4 +1,10 @@
 context do
+  namespace "bin" do
+    Dir['bin/*'].each do |filename|
+      file filename, tags: 'bin'
+    end
+  end
+
   namespace "lib" do
     Dir['lib/**/*.rb'].each do |filename|
       file filename, tags: 'lib'
